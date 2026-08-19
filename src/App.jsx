29e5a9ff0c9a4831c949f5203a7574c796b18bfb
Kidist Meseret <kidistmeseret1@gmail.com>
2026-08-20 08:07:50 +0900

    update app.jsx add the cards ,difficulty,score

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
 function App() {
   return (
-    <div>
-      <h1>Memory Card Game</h1>
+    <div className="app">
+
+      <header>
+        <h1>Memory Card Game</h1>
+
+        <div>
+          <p>Score: 0</p>
+          <p>Best Score: 0</p>
+        </div>
+      </header>
+
+      <main>
+        <h2>Choose Difficulty</h2>
+
+        <button>Easy</button>
+        <button>Medium</button>
+        <button>Hard</button>
+
+        <div className="game-board">
+          <div className="card">
+            <h3>Pikachu</h3>
+          </div>
+
+          <div className="card">
+            <h3>Bulbasaur</h3>
+          </div>
+
+          <div className="card">
+            <h3>Charmander</h3>
+          </div>
+
+          <div className="card">
+            <h3>Squirtle</h3>
+          </div>
+        </div>
+      </main>
+
     </div>
   );
 }
