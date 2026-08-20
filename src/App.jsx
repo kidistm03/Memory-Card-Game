a01a5812472e31253129749318ebf5365aba82c9
Kidist Meseret <kidistmeseret1@gmail.com>
2026-08-20 20:33:46 +0900

    create usestate for clicked cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 function App() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
+  const [clickedCards, setClickedCards] = useState([]);
   function handleCardClick() {
     const newScore = score + 1;
     setScore(newScore);
